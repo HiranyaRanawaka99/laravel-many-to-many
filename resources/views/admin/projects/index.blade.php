247a8655f095cb1c647b88a2ec3d74c6c1f2557c
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-10-30 13:36:06 +0200

    add: delete function

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('content')
 
 <div class="container mt-5">
@@ -25,14 +29,50 @@
       <td> {{ $project->title }}  </td>
       <td>  {!! $project->getCategoryBadge() !!} </td>
       <td> 
-        <a href="{{ route('admin.projects.show', $project)}}" class=""> Show </a>
-        <a href="{{ route('admin.projects.edit', $project)}}" class=""> Modifica </a>
-      </td>
-    </tr>
-    @empty
-      @endforelse
-    </tbody>
-  </table>
+        {{-- SHOW --}}
+        <a href="{{ route('admin.projects.show', $project)}}" class="">  <i class="fa-solid fa-up-right-from-square"></i> </a>
+        {{-- EDIT --}}
+        <a href="{{ route('admin.projects.edit', $project)}}" class="">  <i class="fa-solid fa-pencil"></i> </a>
+        {{-- DELETE --}}
+        <a href="#" data-bs-toggle="modal" data-bs-target="#delete-modal-{{$project->id}}" class="mx-1">
+          <i class="fa-solid fa-trash text-danger"></i>  
+        </a>
+        {{-- Modal --}}
+        <div class="modal fade" id="delete-modal-{{$project->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina progetto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                  Sei sicuro di voler eliminare il progetto con tiolo "{{$project->title}}" con id "{{$project->id}}"?
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                {{-- Delete Form --}}
+                <form action="{{route('admin.projects.destroy', $project)}}" method="POST" class="mx-1">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger">Conferma</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </td>
+  </tr>     
+@empty
+<tr>
+    <td colspan="8">
+        <i>No projects found</i>
+    </td>
+</tr> 
+@endforelse
+</tbody>
+</table>
+
   
   {{ $projects->links('pagination::bootstrap-5') }}
 </div>
